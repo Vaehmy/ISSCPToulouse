ac7d83e81032a1cdfae246fd2db983a8c336cc21
Vaehmy <anais.veeigithub@yopmail.com>
2016-01-09 19:08:19 +0100

    Able to get and save JSON from network !

--- app/src/main/java/com/cap/projet5iss/MainActivity.java
+++ app/src/main/java/com/cap/projet5iss/MainActivity.java
@@ -1,10 +1,19 @@
 package com.cap.projet5iss;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.Toast;
+
 import com.cap.projet5iss.handlers.*;
 import com.cap.projet5iss.handlers.JSONHandler;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,20 +25,33 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Random;
+
+import static android.os.Environment.*;
 
 /**
  * NOTES
@@ -47,6 +69,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private JSONHandler jHAllDrivers ;
     private ArrayList aLAllDrivers ; // Array with all the Driver's JSONObjects
     private VehiclesHandler allVehiHandler ;
+    String jsonFileName;
+    File jsonFile;
 
     protected void onStart(){
         super.onStart();
@@ -69,6 +93,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
+        jsonFileName = "blankFile.json";
+
+        // Creating the directory for our application if it does not already exist
+        File file = new File(Environment.getExternalStorageDirectory(), "/DCToulouse/");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
         userLocalStore= new UserLocalStore(this);
 
     }
@@ -174,54 +205,105 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         aLAllDrivers = jHAllDrivers.jsonFile2ArrayList(getApplicationContext(), "allDrivers.json");
         // Parsing all vehicles
         allVehiHandler = jHAllDrivers.getAllDriversHandler().getVehiclesHandler();
+
+        /**
+         * Exemple steps to get all the drivers' json file from the network :
+         * 1. Set a correct name for the file where we will store the json data
+         * 2. Call AsynTask to perform network operation on separate thread
+         *    with the correct REST url
+         */
+
+        jsonFileName = "allDrivers.json";
+        new HttpAsyncTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
     }
 
 
-    protected void doInBackground(String... params) {
-        String urlString=params[0];
-        String resultToDisplay;
-        InputStream in = null;
+    /***********************************************
+     *               JSON GETTER
+     **********************************************/
 
-        // HTTP Get
+    public String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+        HttpResponse httpResponse = null;
         try {
-            URL url = new URL(urlString);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            in = new BufferedInputStream(urlConnection.getInputStream());
+            httpResponse = httpclient.execute(new HttpGet(url));
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+            if(inputStream != null){
+                // Store inputstream in local file
+                copyInputStreamToFile(inputStream);
+                result = "Worked!";
+            }
+            else {
+                result = "Did not work!";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /**
+     * Method to copy an inputstream into a file with the correct json name
+     * @param in
+     */
+    private void copyInputStreamToFile( InputStream in ) {
+        try {
+            Log.i("MainActivity", "Creating " + jsonFileName + " in " + getExternalStorageDirectory()+"/DCToulouse");
+            File file = new File (getExternalStorageDirectory()+"/DCToulouse", jsonFileName);
+            Log.i("MainActivity", "File was successfully created. Starting to write into " + jsonFileName + "...");
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+            Log.i("MainActivity", "File was successfully written. All streams are closed.");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * To check is the smartphone is connected to the network
+     * @return
+     */
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * We call the GET method in an AsyncTask ( = a separate thread )
+     * so that the application will not freeze while getting the JSON files
+     */
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
         }
 
-        try {
-            File file = new File(getCacheDir(), "cacheFileAppeal.srl");
-            OutputStream output = null;
-            try {
-                output = new FileOutputStream(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            try {
-                try {
-                    byte[] buffer = new byte[4 * 1024]; // or other buffer size
-                    int read;
-
-                    while ((read = in.read(buffer)) != -1) {
-                        output.write(buffer, 0, read);
-                    }
-                    output.flush();
-                } finally {
-                    output.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace(); // handle exception, define IOException and others
-            }
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("JSON GET REQUEST", "Got a result");
         }
-
     }
 
 }
