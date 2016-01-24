18b99ace103a415698150095f5e173157344d4e4
ChaySit <sitayeb.chaymaa@gmail.com>
2016-01-24 18:17:08 +0100

    fonction recherche OK
    sans affichage de list

--- app/src/main/java/com/cap/projet5iss/SearchTraject.java
+++ app/src/main/java/com/cap/projet5iss/SearchTraject.java
@@ -18,6 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by ChaySi on 19/01/2016.
@@ -47,36 +48,117 @@ public class SearchTraject extends Activity {
         this.hDestination = etHeureArrive.getText().toString();
     }
 
+    // fonction qui retourne le nombre de mot dans une phrase
+   /* public int nombreDeMots(String phrase){
+        int i=0;
+        int nbrMot=1;
+        for( i=0; i < phrase.length(); i++){
+            if(phrase.charAt(i) == ' '){ nbrMot++;}
+        }
+        return nbrMot;
+    }*/
+   // Fonction qui me permet de remplir un tableau avec les mots d'une phrase donnée
+   /* public void remplirTabl(String phrase,char motTab[]){
+        int prochainMot=0, i=0, j=0;
+        String buffer="";
+        for(i = 0; i < phrase.length(); i++)
+        {
+            if(phrase.charAt(i) == ' ') // Si on rencontre un espace dans la phrase on change de case car chaque case de motTab correspond à un mot
+            {
+                motTab[j] = '\0';
+                prochainMot++;
+                j = 0;
+            }
+            else if(phrase.charAt(i) != '\0')
+            {
+                motTab[j] = phrase.charAt(i);
+                j++;
+            }
+        }
+        motTab[j] = '\0';
+    }*/
+    
     public void parcourirJSON(){
         jARoutes = jHAllRoutes.getjAallRoutes();
 
         for (int i=0; i<jARoutes.length();i++){
             try {
                 routeObj = jARoutes.getJSONObject(i);
-                if((routeObj.getString("departPlace").equals(adresseDepart))
-                      /* || (routeObj.getString("arrivePlace")== adresseDestination)
-                       || (routeObj.getString("departHour")== hDepart)
-                       || (routeObj.getString("arriveHour")== hDestination)*/)
-                {
-                    Log.i("SEARCH ACTIVITY", "Comparing \""+routeObj.getString("departPlace")+ "\" with \""+adresseDepart+"\" ; MATCH FOUND" );
-                    Log.i("SEARCH ACTIVITY", "Found a match for user #"+routeObj.getString("idUser"));
-                    //Log.i("TROUVE DANS LA BD", "C'est OK redirection");
-                } else{
-                    Log.i("SEARCH ACTIVITY", "Comparing \""+routeObj.getString("departPlace")+ "\" with \""+adresseDepart+"\" ; NO RESULT" );
-                    //Log.i("NE TROUVE RIEN", "C'est OK sauf qu'il trouve rien dans le json");
+
+                String departJSON = routeObj.getString("departPlace");
+                String arriveJSON = routeObj.getString("arrivePlace");
+                String departEdit = adresseDepart;
+                String arriveEdit = adresseDestination;
+
+                ArrayList<String> departJSONMots = new ArrayList<String>(Arrays.asList(departJSON.split(" ")));
+                ArrayList<String> arriveJSONMots = new ArrayList<String>(Arrays.asList(arriveJSON.split(" ")));
+                ArrayList<String> departEditMots = new ArrayList<String>(Arrays.asList(departEdit.split(" ")));
+                ArrayList<String> arriveEditMots = new ArrayList<String>(Arrays.asList(arriveEdit.split(" ")));
+
+                for(int pos = 0; pos < departJSONMots.size(); pos++){
+                    String s1 = (String)departJSONMots.get(pos);
+                    String s2 = (String)departEditMots.get(pos);
+                    String s3 = (String)arriveJSONMots.get(pos);
+                    String s4 = (String)arriveEditMots.get(pos);
+
+                    if( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) )
+                    {
+                        Log.i("SEARCH ACTIVITY", "Comparing \""+routeObj.getString("departPlace")+ "\" with \""+adresseDepart+"\" ; MATCH FOUND" );
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+
+                    }else if( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("departHour").equals(hDepart))){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("arriveHour").equals(hDestination))){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("arriveHour").equals(hDestination)) && (routeObj.getString("departHour").equals(hDepart)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("arriveHour").equals(hDestination)) && ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("arriveHour").equals(hDestination)) && ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) ) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("departHour").equals(hDepart)) && ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( (routeObj.getString("departHour").equals(hDepart)) && ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) ) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) && ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) ) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) && ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) )&& (routeObj.getString("departHour").equals(hDepart)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) && ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) )&& (routeObj.getString("arriveHour").equals(hDestination)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) && (routeObj.getString("departHour").equals(hDepart))&& (routeObj.getString("arriveHour").equals(hDestination)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) ) && (routeObj.getString("departHour").equals(hDepart))&& (routeObj.getString("arriveHour").equals(hDestination)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else if( ( s1.compareTo(s2) == 0 || departJSONMots.contains(s2) ) && ( s3.compareTo(s4) == 0 || arriveJSONMots.contains(s3)) && (routeObj.getString("departHour").equals(hDepart))&& (routeObj.getString("arriveHour").equals(hDestination)) ){
+                        Log.i("SEARCH ACTIVITY", "Found a match for user #" + routeObj.getString("idUser"));
+                    }
+                    else {
+                        Log.i("SEARCH ACTIVITY", "Comparing \""+routeObj.getString("departPlace")+ "\" with \""+adresseDepart+"\" ; NO RESULT" );
+
+                    }
                 }
-
-                // toute comparaison et voir ce que ça affiche comme résultats
-                // Gestion d'erreur
-
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
-
-
     }
 
     public String rechercher(){
